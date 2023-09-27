4391cbe39859b620f53e1fea760d262184fbfe35
codewthjava <abhisunnii@gmail.com>
2023-09-27 10:17:54 +0530

    folder structure,angular material setup changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './shared/components/login/login.component';
+import { CreateAccountComponent } from './shared/components/create-account/create-account.component';
+import { LandingComponent } from './shared/components/landing/landing.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { ToolbarComponent } from './shared/components/toolbar/toolbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+
+import { SharedModule } from './shared/module/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CreateAccountComponent,
+    LandingComponent,
+    NavbarComponent,
+    ToolbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
